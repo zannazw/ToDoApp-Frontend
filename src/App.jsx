4f697c1281500649a306b10a04484f58703eecf9
zannazw <zanna.wahedi@outlook.com>
2025-04-13 16:14:46 +0200

    add fetch mechanism

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,25 @@
 import './App.css'
 import ToDo from "./components/ToDo/ToDo.jsx";
+import {useEffect, useState} from "react";
+import axios from "axios";
 
 function App() {
+    const [todos, setTodos] = useState([]);
 
-    const num = 3;
+    useEffect(() => {
+        axios.get(import.meta.env.VITE_BACKEND_URL + "/todo")
+            .then(response => {
+                setTodos(response.data)
+                console.log(response.data);
+                console.log("Hat Todos gefetched und in den State gepackt :)")
+            })
+            .catch(error => console.error(error));
+    }, []);
 
     return (
         <>
             <div className={"header-container"}>
-                Hallo
+                <h1>Meine To-Do Liste</h1>
                 <button className={"add-button"}>
                     +
                 </button>
